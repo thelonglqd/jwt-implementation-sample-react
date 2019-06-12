8fadf3a62f4ad6e4d9054cc7b36bd9a3ea27f258
Nguyen The Phung Long (NWA.AT) <LongNTP@fsoft.com.vn>
2019-06-12 14:16:05 +0700

    add ant design login form

--- src/index.js
+++ src/index.js
@@ -1,12 +1,85 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Form, Icon, Input, Button } from "antd";
+import "antd/dist/antd.css";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function hasErrors(fieldsError) {
+  return Object.keys(fieldsError).some(field => fieldsError[field]);
+}
+
+class App extends React.Component {
+  componentDidMount() {
+    // To disabled submit button at the beginning.
+    this.props.form.validateFields();
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log("Received values of form: ", values);
+      }
+    });
+  };
+
+  render() {
+    const {
+      getFieldDecorator,
+      getFieldsError,
+      getFieldError,
+      isFieldTouched
+    } = this.props.form;
+
+    // Only show error after a field is touched.
+    const usernameError =
+      isFieldTouched("username") && getFieldError("username");
+    const passwordError =
+      isFieldTouched("password") && getFieldError("password");
+
+    return (
+      <Form layout="inline" onSubmit={this.handleSubmit}>
+        <Form.Item
+          validateStatus={usernameError ? "error" : ""}
+          help={usernameError || ""}
+        >
+          {getFieldDecorator("username", {
+            rules: [{ required: true, message: "Please input your username!" }]
+          })(
+            <Input
+              prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
+              placeholder="Username"
+            />
+          )}
+        </Form.Item>
+        <Form.Item
+          validateStatus={passwordError ? "error" : ""}
+          help={passwordError || ""}
+        >
+          {getFieldDecorator("password", {
+            rules: [{ required: true, message: "Please input your Password!" }]
+          })(
+            <Input
+              prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />}
+              type="password"
+              placeholder="Password"
+            />
+          )}
+        </Form.Item>
+        <Form.Item>
+          <Button
+            type="primary"
+            htmlType="submit"
+            disabled={hasErrors(getFieldsError())}
+          >
+            Log in
+          </Button>
+        </Form.Item>
+      </Form>
+    );
+  }
+}
+
+const WrappedLoginForm = Form.create({ name: "horizontal_login" })(App);
+
+ReactDOM.render(<WrappedLoginForm />, document.querySelector("#root"));
